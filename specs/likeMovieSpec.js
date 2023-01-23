9556d0383d4de7c79d7568d61c204c7a11c23ab7
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-24 02:01:28 +0700

    create integration testing in like movie

---
+++ specs/likeMovieSpec.js
@@ -0,0 +1,84 @@
+/* eslint-disable comma-dangle */
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
+
+describe('Liking a Movie', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(() => {
+    addLikeButtonContainer();
+  });
+
+  it('Should show the like button when the movie has not been liked before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(
+      document.querySelector('[aria-label="like this movie"]')
+    ).toBeTruthy();
+  });
+
+  it('Should not show the unlike button when the movie has not been liked before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(
+      document.querySelector('[aria-label="unlike this movie"]')
+    ).toBeFalsy();
+  });
+
+  it('Should be able to like this movie', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+    const movie = await FavoriteMovieIdb.getMovie(1);
+    expect(movie).toEqual({ id: 1 });
+
+    FavoriteMovieIdb.deleteMovie(1);
+  });
+
+  it('Should not add a movie again when its already liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    // tambahkan film dengan ID 1 ke daftar film yang disukai
+    await FavoriteMovieIdb.putMovie({ id: 1 });
+
+    // simulasikan pengguna menekan tombol suka film
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    // tidak ada film yang ganda
+    expect(await FavoriteMovieIdb.getAllMovies()).toEqual([{ id: 1 }]);
+
+    FavoriteMovieIdb.deleteMovie(1);
+  });
+
+  xit('Should not add a movie when it has no id', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {},
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+    expect(await FavoriteMovieIdb.getAllMovies()).toEqual([]);
+  });
+});
